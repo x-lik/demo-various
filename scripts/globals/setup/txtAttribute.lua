59f3ceb72a271ae92ebb358f70d6cfd452359710
hunzsig <mzyhaohaoren@qq.com>
2024-04-30 16:02:14 +0800

    fixed(*): bugs

--- scripts/globals/setup/txtAttribute.lua
+++ scripts/globals/setup/txtAttribute.lua
@@ -2,20 +2,20 @@
 
 --- 游戏规定的几率key
 local odds = {
-    "_hurtRebound",
-    "_crit",
-    "_stun"
+    "hurtRebound",
+    "crit",
+    "stun"
 }
 --- 游戏规定的抗性key
 local resistance = {
-    "_hpSuckAttack",
-    "_hpSuckAbility",
-    "_mpSuckAttack",
-    "_mpSuckAbility",
-    "_hurtRebound",
-    "_crit",
-    "_stun",
-    "_punish",
+    "hpSuckAttack",
+    "hpSuckAbility",
+    "mpSuckAttack",
+    "mpSuckAbility",
+    "hurtRebound",
+    "crit",
+    "stun",
+    "punish",
     "split",
     "silent",
     "unArm",
@@ -39,65 +39,65 @@ local elementIconPath = {
 
 local T = true
 local F = false
-attribute.conf(F, "_str", "力量", nil, "ability/Strength3")
-attribute.conf(F, "_agi", "敏捷", nil, "ability/RogueQuickRecovery")
-attribute.conf(F, "_int", "智力", nil, "ability/DeclarationofGod")
-attribute.conf(F, "_aim", "命中", '%', "ability/DeadlyDoubleLine")
-attribute.conf(F, "_animateScale", "动作", '%', "ability/LightWork")
-attribute.conf(F, "_attack", "攻击", nil, "ability/CrystalSlash")
-attribute.conf(F, "_attackRange", "攻击范围", nil, "ability/HunterLockAndLoad")
-attribute.conf(F, "_attackRangeAcquire", "主动攻击范围", nil, "ability/HunterLockAndLoad")
-attribute.conf(F, "_attackRipple", "攻击浮动")
-attribute.conf(T, "_attackSpace", "攻击间隔", "击每秒", "ability/WarriorWeaponMastery")
-attribute.conf(T, "_attackSpaceBase", "原始攻击间隔", "击每秒", "ability/WarriorWeaponMastery")
-attribute.conf(F, "_attackSpeed", "攻击速度", '%', "ability/DemonhunterBladeDance")
-attribute.conf(F, "_avoid", "回避", '%', "ability/Greenphantom")
-attribute.conf(F, "_breakArmor", "破甲", nil, "ability/ShadowCurseOfSargeras")
-attribute.conf(T, "_castChant", "吟唱时间", "秒", "ability/ShadowLastingAfflictions")
-attribute.conf(T, "_castChantPercent", "吟唱时间", '%', "ability/ShadowLastingAfflictions")
-attribute.conf(F, "_castDistance", "施法距离", nil, "ability/ShadowLastingAfflictions")
-attribute.conf(F, "_castDistancePercent", "施法距离", '%', "ability/ShadowLastingAfflictions")
-attribute.conf(T, "_castKeep", "施法持续时间", "秒", "ability/ShadowLastingAfflictions")
-attribute.conf(T, "_castKeepPercent", "施法持续时间", '%', "ability/ShadowLastingAfflictions")
-attribute.conf(F, "_castRange", "施法范围", nil, "ability/ShadowLastingAfflictions")
-attribute.conf(F, "_castRangePercent", "施法范围", '%', "ability/ShadowLastingAfflictions")
-attribute.conf(T, "_coolDown", "冷却时间", "秒", "ability/ShadowLastingAfflictions")
-attribute.conf(T, "_coolDownPercent", "冷却时间", '%', "ability/ShadowLastingAfflictions")
-attribute.conf(T, "_cost", "施法消耗", nil, "ability/ShadowLastingAfflictions")
-attribute.conf(T, "_costPercent", "施法消耗", '%', "ability/ShadowLastingAfflictions")
-attribute.conf(F, "_crit", "暴击", '%', "ability/ShockCut")
-attribute.conf(F, "_cure", "治疗加成", '%', "ability/Heal")
-attribute.conf(F, "_damageIncrease", "伤害增幅", '%', "ability/LingerSometimesConstantlyKnife")
-attribute.conf(F, "_defend", "防御", nil, "ability/ThickFur")
-attribute.conf(F, "_enchantMystery", "附魔精通", '%', "ability/ElementalSilk")
-attribute.conf(F, "_freeze", "冻结", nil, "ability/HolyWrath")
-attribute.conf(F, "_hp", "HP", nil, "ability/HPRecharge")
-attribute.conf(F, "_hpCur", "当前HP", nil, "ability/HPRecharge")
-attribute.conf(F, "_hpRegen", "HP恢复", "每秒", "ability/ReplenishHealth")
-attribute.conf(F, "_hpSuckAbility", "技能吸血", '%', "ability/DrainLife")
-attribute.conf(F, "_hpSuckAttack", "攻击吸血", '%', "ability/NatureHealingWay")
-attribute.conf(T, "_hurtIncrease", "受伤加深", '%', "ability/IronmaidensBloodritual")
-attribute.conf(F, "_hurtRebound", "反弹伤害", '%', "ability/MageMoltenArmor")
-attribute.conf(F, "_hurtReduction", "受伤减轻", '%', "ability/HolyGreaterBlessingofSanctuary")
-attribute.conf(F, "_move", "移动", nil, "ability/RogueFleetFooted")
-attribute.conf(F, "_mp", "MP", nil, "ability/ManaRecharge2")
-attribute.conf(F, "_mpCur", "当前MP", nil, "ability/ManaRecharge2")
-attribute.conf(F, "_mpRegen", "MP恢复", "每秒", "ability/ReplenishMana")
-attribute.conf(F, "_mpSuckAbility", "技能吸魔", '%', "ability/DrainMana")
-attribute.conf(F, "_mpSuckAttack", "攻击吸魔", '%', "ability/ManaBurn2")
-attribute.conf(F, "_nsight", "夜晚视野", nil, "ability/SigntNight")
-attribute.conf(F, "_reborn", "复活时间", "秒", "ability/HolyHolyGuidance")
-attribute.conf(F, "_rgba", "偏色", nil, "ability/HunterBeastWithin")
-attribute.conf(F, "_punish", "硬直", nil, "ability/ValkyrieArmed")
-attribute.conf(F, "_punishCur", "当前硬直", nil, "ability/ValkyrieArmed")
-attribute.conf(F, "_punishRegen", "硬直恢复", "每秒", "ability/GodofWar")
-attribute.conf(F, "_shield", "护盾", nil, "ability/HolyBlessingOfProtection")
-attribute.conf(T, "_shieldBack", "护盾恢复需时", "秒", "ability/HolyBlessingOfProtection")
-attribute.conf(F, "_sight", "白昼视野", nil, "ability/SigntDay")
-attribute.conf(F, "_stun", "眩晕", '%', "ability/Stun")
-attribute.conf(F, "_visible", "反隐范围", nil, "ability/MagicalSentry")
-attribute.conf(F, "_weight", "负重", nil, "other/DustOfAppearance")
-attribute.conf(F, "_weightCur", "当前负重", nil, "other/DustOfAppearance")
+attribute.conf(F, "str", "力量", nil, "ability/Strength3")
+attribute.conf(F, "agi", "敏捷", nil, "ability/RogueQuickRecovery")
+attribute.conf(F, "int", "智力", nil, "ability/DeclarationofGod")
+attribute.conf(F, "aim", "命中", '%', "ability/DeadlyDoubleLine")
+attribute.conf(F, "animateScale", "动作", '%', "ability/LightWork")
+attribute.conf(F, "attack", "攻击", nil, "ability/CrystalSlash")
+attribute.conf(F, "attackRange", "攻击范围", nil, "ability/HunterLockAndLoad")
+attribute.conf(F, "attackRangeAcquire", "主动攻击范围", nil, "ability/HunterLockAndLoad")
+attribute.conf(F, "attackRipple", "攻击浮动")
+attribute.conf(T, "attackSpace", "攻击间隔", "击每秒", "ability/WarriorWeaponMastery")
+attribute.conf(T, "attackSpaceBase", "原始攻击间隔", "击每秒", "ability/WarriorWeaponMastery")
+attribute.conf(F, "attackSpeed", "攻击速度", '%', "ability/DemonhunterBladeDance")
+attribute.conf(F, "avoid", "回避", '%', "ability/Greenphantom")
+attribute.conf(F, "breakArmor", "破甲", nil, "ability/ShadowCurseOfSargeras")
+attribute.conf(T, "castChant", "吟唱时间", "秒", "ability/ShadowLastingAfflictions")
+attribute.conf(T, "castChantPercent", "吟唱时间", '%', "ability/ShadowLastingAfflictions")
+attribute.conf(F, "castDistance", "施法距离", nil, "ability/ShadowLastingAfflictions")
+attribute.conf(F, "castDistancePercent", "施法距离", '%', "ability/ShadowLastingAfflictions")
+attribute.conf(T, "castKeep", "施法持续时间", "秒", "ability/ShadowLastingAfflictions")
+attribute.conf(T, "castKeepPercent", "施法持续时间", '%', "ability/ShadowLastingAfflictions")
+attribute.conf(F, "castRange", "施法范围", nil, "ability/ShadowLastingAfflictions")
+attribute.conf(F, "castRangePercent", "施法范围", '%', "ability/ShadowLastingAfflictions")
+attribute.conf(T, "coolDown", "冷却时间", "秒", "ability/ShadowLastingAfflictions")
+attribute.conf(T, "coolDownPercent", "冷却时间", '%', "ability/ShadowLastingAfflictions")
+attribute.conf(T, "cost", "施法消耗", nil, "ability/ShadowLastingAfflictions")
+attribute.conf(T, "costPercent", "施法消耗", '%', "ability/ShadowLastingAfflictions")
+attribute.conf(F, "crit", "暴击", '%', "ability/ShockCut")
+attribute.conf(F, "cure", "治疗加成", '%', "ability/Heal")
+attribute.conf(F, "damageIncrease", "伤害增幅", '%', "ability/LingerSometimesConstantlyKnife")
+attribute.conf(F, "defend", "防御", nil, "ability/ThickFur")
+attribute.conf(F, "enchantMystery", "附魔精通", '%', "ability/ElementalSilk")
+attribute.conf(F, "freeze", "冻结", nil, "ability/HolyWrath")
+attribute.conf(F, "hp", "HP", nil, "ability/HPRecharge")
+attribute.conf(F, "hpCur", "当前HP", nil, "ability/HPRecharge")
+attribute.conf(F, "hpRegen", "HP恢复", "每秒", "ability/ReplenishHealth")
+attribute.conf(F, "hpSuckAbility", "技能吸血", '%', "ability/DrainLife")
+attribute.conf(F, "hpSuckAttack", "攻击吸血", '%', "ability/NatureHealingWay")
+attribute.conf(T, "hurtIncrease", "受伤加深", '%', "ability/IronmaidensBloodritual")
+attribute.conf(F, "hurtRebound", "反弹伤害", '%', "ability/MageMoltenArmor")
+attribute.conf(F, "hurtReduction", "受伤减轻", '%', "ability/HolyGreaterBlessingofSanctuary")
+attribute.conf(F, "move", "移动", nil, "ability/RogueFleetFooted")
+attribute.conf(F, "mp", "MP", nil, "ability/ManaRecharge2")
+attribute.conf(F, "mpCur", "当前MP", nil, "ability/ManaRecharge2")
+attribute.conf(F, "mpRegen", "MP恢复", "每秒", "ability/ReplenishMana")
+attribute.conf(F, "mpSuckAbility", "技能吸魔", '%', "ability/DrainMana")
+attribute.conf(F, "mpSuckAttack", "攻击吸魔", '%', "ability/ManaBurn2")
+attribute.conf(F, "nsight", "夜晚视野", nil, "ability/SigntNight")
+attribute.conf(F, "reborn", "复活时间", "秒", "ability/HolyHolyGuidance")
+attribute.conf(F, "rgba", "偏色", nil, "ability/HunterBeastWithin")
+attribute.conf(F, "punish", "硬直", nil, "ability/ValkyrieArmed")
+attribute.conf(F, "punishCur", "当前硬直", nil, "ability/ValkyrieArmed")
+attribute.conf(F, "punishRegen", "硬直恢复", "每秒", "ability/GodofWar")
+attribute.conf(F, "shield", "护盾", nil, "ability/HolyBlessingOfProtection")
+attribute.conf(T, "shieldBack", "护盾恢复需时", "秒", "ability/HolyBlessingOfProtection")
+attribute.conf(F, "sight", "白昼视野", nil, "ability/SigntDay")
+attribute.conf(F, "stun", "眩晕", '%', "ability/Stun")
+attribute.conf(F, "visible", "反隐范围", nil, "ability/MagicalSentry")
+attribute.conf(F, "weight", "负重", nil, "other/DustOfAppearance")
+attribute.conf(F, "weightCur", "当前负重", nil, "other/DustOfAppearance")
 attribute.conf(F, "unArm", "缴械", nil, "ability/ShadowCurseOfMannoroth")
 attribute.conf(F, "silent", "沉默", nil, "ability/Silence")
 attribute.conf(F, "split", "分裂", nil, "ability/CleavingAttack")
