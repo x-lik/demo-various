7bbfca9ee3d725b8755cd364db269a20fd243266
hunzsig <mzyhaohaoren@qq.com>
2024-05-12 09:24:28 +0800

    feat(test): *

---
+++ scripts/process/cale.lua
@@ -0,0 +1,15 @@
+local process = Process("cale")
+
+process:onStart(function(_)
+    
+    
+    print(math.cale("*=0.7", 10))
+    print(math.cale("/=0.7", 10))
+    print(math.cale("+=0.7", 10))
+    print(math.cale("-=0.7", 10))
+    print(math.cale("*=1.7", 10))
+    print(math.cale("/=1.7", 10))
+    print(math.cale("*=-0.3", 10))
+    print(math.cale("*=-8", 10))
+
+end)
