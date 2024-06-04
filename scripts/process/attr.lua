df5d9a6bd7713e99ce03ecccc1463b735be470b7
hunzsig <mzyhaohaoren@qq.com>
2024-06-04 16:05:47 +0800

    feat(*): *

--- scripts/process/attr.lua
+++ scripts/process/attr.lua
@@ -8,17 +8,17 @@ function process:onStart()
     local u = Unit(Player(1), TPL_UNIT.Footman, 0, -200, 270)
     bubble.u1 = u
     
-    print("hp1", u:hp(), u:get("hp", SYMBOL_AMP))
+    print("hp1", u:hp(), u:ampl("hp"))
     u:hp("+=100")
-    print("hp2", u:hp(), u:get("hp", SYMBOL_AMP))
+    print("hp2", u:hp(), u:ampl("hp"))
     u:ampl("hp", "+=100")
-    print("hp3", u:hp(), u:get("hp", SYMBOL_AMP))
+    print("hp3", u:hp(), u:ampl("hp"))
     u:hp("+=100")
-    print("hp4", u:hp(), u:get("hp", SYMBOL_AMP))
+    print("hp4", u:hp(), u:ampl("hp"))
     u:ampl("hp", "+=100")
-    print("hp5", u:hp(), u:get("hp", SYMBOL_AMP))
+    print("hp5", u:hp(), u:ampl("hp"))
     u:raise("hp", "+=100")
     u:hp("+=333")
-    print("hp6", u:hp(), u:get("hp", SYMBOL_RAI))
+    print("hp6", u:hp(), u:raise("hp"))
 
 end
