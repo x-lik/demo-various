31809cddc65da4f25807bdda39a7c1f584802fcb
hunzsig <mzyhaohaoren@qq.com>
2024-06-09 07:59:13 +0800

    feat(*): *

--- scripts/process/reborn.lua
+++ scripts/process/reborn.lua
@@ -4,9 +4,9 @@ function process:onStart()
     
     local bubble = self:bubble()
     
-    for i = 1, 1 do
+    for i = 1, 5 do
         local u1 = Unit(Player(1), TPL_UNIT.Footman, 0, -1000, 66.6):reborn(2)
-        u1:period(1)
+        u1:period(5)
         bubble["u" .. i] = u1
     end
 
