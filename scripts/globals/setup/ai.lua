3ef68f300dd00673db1d17860873f62a3863f30d
hunzsig <mzyhaohaoren@qq.com>
2024-09-01 13:06:59 +0800

    feat(AI): *

--- scripts/globals/setup/ai.lua
+++ scripts/globals/setup/ai.lua
@@ -2,20 +2,26 @@
 
 --- [游荡进攻型AI]
 --- 每隔5秒选择攻击到附近350范围内随机坐标
-AI("wander", {
-    period = 5,
-    action = function(linkUnit)
+--- 停顿2秒后再次攻击
+AI("wander"):behavior("idle", function(this, linkUnit)
+    time.setTimeout(5, function()
         local x, y = vector2.polar(linkUnit:x(), linkUnit:y(), math.rand(50, 350), math.rand(0, 359))
         linkUnit:orderAttack(x, y)
-    end
-})
+        time.setTimeout(2, function()
+            this:action("idle", linkUnit)
+        end)
+    end)
+end)
 
 --- [游荡移动型AI]
 --- 每隔5秒选择移动到附近350范围内随机坐标
-AI("loiter", {
-    period = 5,
-    action = function(linkUnit)
+--- 停顿2秒后再次移动
+AI("loiter"):behavior("idle", function(this, linkUnit)
+    time.setTimeout(5, function()
         local x, y = vector2.polar(linkUnit:x(), linkUnit:y(), math.rand(50, 350), math.rand(0, 359))
         linkUnit:orderAIMove(x, y)
-    end
-})
+        time.setTimeout(2, function()
+            this:action("idle", linkUnit)
+        end)
+    end)
+end)
