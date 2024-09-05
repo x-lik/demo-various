de9ccbe9f6443c821b5b95ef13ea3dcd92cfae05
hunzsig <mzyhaohaoren@qq.com>
2024-09-05 17:07:26 +0800

    feat(*): 新增测试

---
+++ scripts/process/moveStop.lua
@@ -0,0 +1,16 @@
+local process = Process("moveStop")
+
+function process:onStart()
+    
+    local bubble = self:bubble()
+    
+    local u1 = Unit(Player(1), TPL_UNIT.HeroFlameLord, 0, 0, 0)
+    
+    ---@param evtData evtOnUnitMoveStopData
+    u1:onEvent(eventKind.unitMoveStop, "test", function(evtData)
+        print("途径距离", evtData.distance)
+    end)
+    
+    bubble.u1 = u1
+
+end
