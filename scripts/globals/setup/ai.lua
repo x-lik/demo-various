e3a493ed5e71c0d6c7f50f67fb241bc4cc7bdfa7
hunzsig <mzyhaohaoren@qq.com>
2024-09-06 20:08:28 +0800

    feat(AI): *

--- scripts/globals/setup/ai.lua
+++ scripts/globals/setup/ai.lua
@@ -2,26 +2,24 @@
 
 --- [游荡进攻型AI]
 --- 每隔5秒选择攻击到附近350范围内随机坐标
---- 停顿2秒后再次攻击
 AI("wander"):behavior("idle", function(this, linkUnit)
     time.setTimeout(5, function()
         local x, y = vector2.polar(linkUnit:x(), linkUnit:y(), math.rand(50, 350), math.rand(0, 359))
         linkUnit:orderAttack(x, y)
-        time.setTimeout(2, function()
-            this:action("idle", linkUnit)
-        end)
     end)
 end)
 
 --- [游荡移动型AI]
---- 每隔5秒选择移动到附近350范围内随机坐标
---- 停顿2秒后再次移动
+--- 随机选择移动到附近350范围内随机坐标
+--- 到达后停顿3秒后再次移动
 AI("loiter"):behavior("idle", function(this, linkUnit)
-    time.setTimeout(5, function()
-        local x, y = vector2.polar(linkUnit:x(), linkUnit:y(), math.rand(50, 350), math.rand(0, 359))
-        linkUnit:orderAIMove(x, y)
-        time.setTimeout(2, function()
-            this:action("idle", linkUnit)
+    ---@param evtData evtOnUnitMoveStopData
+    linkUnit:onEvent(eventKind.unitMoveStop, "loiter", function(evtData)
+        linkUnit:onEvent(eventKind.unitMoveStop, "loiter", nil)
+        time.setTimeout(3, function()
+            this:action("idle", evtData.triggerUnit)
         end)
     end)
+    local x, y = vector2.polar(linkUnit:x(), linkUnit:y(), math.rand(50, 350), math.rand(0, 359))
+    linkUnit:orderAIMove(x, y)
 end)
