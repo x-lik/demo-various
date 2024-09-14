7b6eb9c6375a04306d34efe18116bf9a752f8f28
hunzsig <mzyhaohaoren@qq.com>
2024-09-14 14:55:14 +0800

    feat(*): *

--- assets/sound.lua
+++ assets/sound.lua
@@ -1,13 +1,9 @@
 --【 音效 】
 
 assets_bgm("BloonsTD6 - Fiesta Flamenco", "td6")
-assets_bgm("Music\\mp3Music\\Human1.mp3", "Human1")
-assets_bgm("war3mapImports\\Phigma - 珊瑚海.mp3", "Phigma1")
+assets_bgm("Sound\\Music\\mp3Music\\Human1.mp3", "Human1")
 
-assets_vcm("Sound\\Interface\\AutoCastButtonClick1.wav", "war3_AutoCastButtonClick1")
-assets_vcm("Sound\\Interface\\CreepAggroWhat1.wav", "war3_CreepAggroWhat1")
 assets_vcm("Sound\\Interface\\Error.wav", "war3_Error")
-assets_vcm("Sound\\Interface\\GoodJob.wav", "war3_GoodJob")
 assets_vcm("Sound\\Interface\\HeroDropItem1.wav", "war3_HeroDropItem1")
 assets_vcm("Sound\\Interface\\MapPing.wav", "war3_MapPing")
 assets_vcm("Sound\\Interface\\MouseClick1.wav", "war3_MouseClick1")
