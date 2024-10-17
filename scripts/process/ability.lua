120fc5321fd5e5c71a063edb3017e5ab6c4a9f25
hunzsig <mzyhaohaoren@qq.com>
2024-10-17 15:13:23 +0800

    feat(*): update

--- scripts/process/ability.lua
+++ scripts/process/ability.lua
@@ -1,30 +1,53 @@
 local process = Process("ability")
 
 function process:onStart()
-    local u1 = Unit(Player(1), TPL_UNIT.HeroFlameLord, 500, -500, 66.6)
-        :level(1)
-        :reborn(0.5)
-        :hp(100)
-        :hpRegen(10)
-        :mp(100)
-        :mpRegen(-1)
-        :move(522)
-        :assault(Assault({ mode = "missile", model = "PriestMissile", homing = true, height = 300, speed = 500 }))
-        :attackSpaceBase(1)
-        :attack(91)
-        :attackRange(1000)
-        :attackSpeed(100)
-        :crit(10):odds("crit", 10)
-        :hpSuckAttack("+=10")
-        :mpSuckAttack("+=10")
-        :bindItemTpl()
-    
-    u1:odds("hurtRebound", "+=100")
-    
-    u1:abilitySlot():push(TPL_ABILITY.AB1)
-    u1:abilitySlot():push(TPL_ABILITY.AB2)
-    u1:abilitySlot():push(TPL_ABILITY.King, 6)
-    
     local bubble = self:bubble()
+    local u1 = Unit(Player(1), TPL_UNIT.DarkRanger, 0, 0, 270)
     bubble.u1 = u1
+    
+    --ability.crackFly({
+    --    targetUnit = u1,
+    --    frequency = 0.03, --[可选]刷新周期，默认0.03
+    --    distance = 200, --[可选]击退距离，默认0
+    --    height = 200, --[可选]飞跃高度，默认100
+    --    duration = 1, --[必须]击飞过程持续时间，可选，默认0.5秒
+    --    bounce = { qty = 3, height = 0.5 }
+    --})
+    --ability.leap({
+    --    sourceUnit = u1,
+    --    targetVec = { 1500, -600 },
+    --    frequency = 0.03,
+    --    speed = 100,
+    --    height = 200,
+    --    acceleration = 50,
+    --})
+    --ability.serpentine({
+    --    sourceUnit = u1,
+    --    model = "CrushingWaveMissile",
+    --    targetVec = { 0, -1500 },
+    --    frequency = 0.02,
+    --    speed = 350,
+    --    animate = 8,
+    --    acceleration = 10,
+    --    point1 = 0.3,
+    --    offset1 = 300,
+    --    point2 = 0.6,
+    --    offset2 = 300,
+    --})
+    local num = 1000
+    for _ = 1, num do
+        local x1, y1 = math.rand(-3000, -1500), math.rand(-2000, 2000)
+        local x2, y2 = math.rand(1500, 3000), math.rand(-2000, 2000)
+        local h = math.rand(800, 1000)
+        ability.missile({
+            model = "ProcMissile",
+            sourceVec = { x1, y1 },
+            targetVec = { x2, y2 },
+            frequency = 0.02,
+            speed = 1000,
+            height = h,
+            acceleration = 0,
+        })
+    end
+
 end
