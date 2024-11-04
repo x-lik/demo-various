fbb41154268c00cae26c4c67534bae99df3a2524
hunzsig <mzyhaohaoren@qq.com>
2024-11-04 15:09:20 +0800

    feat(amass): update demo

--- scripts/process/amass.lua
+++ scripts/process/amass.lua
@@ -5,6 +5,8 @@ function process:onStart()
     local bubble = self:bubble()
     
     local u1 = Unit(Player(1), TPL_UNIT.DarkRanger, 0, 0, 270)
+    u1:barStateDisplay(2)
+    u1:barStateMode(1)
     u1:hp(10000)
     u1:abilitySlot():insert(TPL_ABILITY.AB_Amass)
     bubble.u1 = u1
