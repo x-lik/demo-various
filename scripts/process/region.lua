9c2032d112614e408bedefc2792ff44d222b5bb7
hunzsig <mzyhaohaoren@qq.com>
2024-11-25 19:36:02 +0800

    feat(*): update

--- scripts/process/region.lua
+++ scripts/process/region.lua
@@ -3,7 +3,7 @@ local process = Process("region")
 function process:onStart()
     
     local r = Region("中心判断区", "square", 0, 0, 500, 500)
-    r:splat(BLP_COLOR_BLUE, 200)
+    r:splat("square_white", 200)
     r:onEnter(function(evtData)
         print("Enter")
         print(evtData.triggerUnit:name() .. "进入了" .. evtData.triggerRegion:name())
